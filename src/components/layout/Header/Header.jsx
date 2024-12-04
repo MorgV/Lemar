452fa8ea4018f4e9de5cf35fa0af25622534ecd3
MorgV <erikhan03@mail.ru>
2024-12-04 15:37:13 +0300

    models start upgrade

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
 					<nav className={burgerState ? 'header__menu active' : 'header__menu'}>
 						<ul className='header__list'>
 							<li>
-								<a href='' className='header__link'>
+								<a href='/Lemar/models' className='header__link'>
 									База моделей
 								</a>
 							</li>
--- src/components/screens/Models/Models.jsx
+++ src/components/screens/Models/Models.jsx
@@ -1,12 +1,22 @@
 import Layout from '../../layout/Layout'
 // import styles from './Models.module.scss';
+import Card from './ModelsUI/Card/Card'
+import cardsData from '../../../data/data.json'
 
 function Models() {
 	// const { isAuth } = useAuth()
 	// const navigate = useNavigate()
 	return (
 		<Layout>
-			<div className=''>Models</div>
+			<main className='main'>
+				<div className='container'>
+					<div className='main__cards'>
+						{cardsData.map((item, index) => {
+							return <Card data={item} key={index} />
+						})}
+					</div>
+				</div>
+			</main>
 		</Layout>
 	)
 }
