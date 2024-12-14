3f114cbcfc9111f40065933d75850678df3aa287
MorgV <erikhan03@mail.ru>
2024-12-14 09:43:18 +0300

    ReactQueryDevtools add

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './assets/styles/index.scss'
 import Router from './shared/routes/routes'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { queryClient } from './shared/api/query-client'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<Router />
+			<ReactQueryDevtools />
 		</QueryClientProvider>
 	</StrictMode>
 )
